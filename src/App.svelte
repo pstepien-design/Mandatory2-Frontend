dcf7bd3db0b9231400d2d107be6eda59787aaa28
pstepien-design <pstepien@interia.pl>
2022-04-10 17:01:25 +0200

    added header and navbar

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Header from './components/Header.svelte';
+  import Navbar from './components/Navbar.svelte';
+</script>
+
+<main>
+  <Header />
+	<Navbar />
+</main>
+
+<style>
+  :global(body) {
+    padding: 0;
+		font-family: Roboto;
+  }
+  main {
+    text-align: center;
+    max-width: 240px;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,21 @@
+<div>
+  <p>WEBSHOP
+    </p>
+  </div>
+
+<style>
+  div {
+    height: 50px;
+    background-color: #E5E5E5;
+    color: #000000;
+    width: 100%;
+    text-align: center;
+    margin: 0;
+    
+   
+  }
+  p{
+    margin: 0;
+    font-size: 30px;
+  }
+</style>
